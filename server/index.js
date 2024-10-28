6e3f8dc202f3f0dd1fb8f2b93ecb0f7164bef053
mailbau <muhammad.ismail.azmi@mail.ugm.ac.id>
2024-10-28 15:06:57 +0700

    fix: renaming

--- server/index.js
+++ server/index.js
@@ -2,13 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const listEndpoints = require('express-list-endpoints');
 require('dotenv').config();
+const db = require('./config/firebase');
 
 const userRouter = require('./routers/userRouter');
 const tpsRouter = require('./routers/tpsRouter');
 const pathRouter = require('./routers/pathRouter');
 const sensorRouter = require('./routers/sensorRouter');
+const authRouter = require('./routers/authRouter');
+const authenticateJWT = require('./middleware/authMiddleware');
 
 const app = express();
+
+// Test the connection to Firebase
+db.ref('/').get().then(() => {
+    console.log('Connected to Firebase');
+}).catch((error) => {
+    console.error('Error connecting to Firebase', error);
+});
 
 // Enable CORS
 app.use(cors());
@@ -16,7 +26,10 @@ app.use(cors());
 // Use JSON parsing middleware
 app.use(express.json());
 
-// Routes
+// Use the auth router
+app.use('/auth', authRouter);
+
+// Protected routes
 app.use('/user', userRouter);
 app.use('/tps', tpsRouter);
 app.use('/path', pathRouter);
--- server/routers/pathRouter.js
+++ server/routers/pathRouter.js
@@ -2,19 +2,19 @@ const express = require('express');
 const pathController = require('../controllers/pathController');
 const router = express.Router();
 
-// Get all routes
+// Get all paths
 router.get('/', pathController.getAllPaths);
 
-// Add route
+// Add path
 router.post('/register', pathController.addPath);
 
-// Get specific route by ID
+// Get specific path by ID
 router.get('/:pathId', pathController.getPathById);
 
-// Update route by ID
+// Update path by ID
 router.put('/:pathId', pathController.updatePath);
 
-// Delete route by ID
+// Delete path by ID
 router.delete('/:pathId', pathController.deletePath);
 
 module.exports = router;
