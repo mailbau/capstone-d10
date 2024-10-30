d6c40414442e7b02a9ee9c4cd7f8ce578ad415d1
mailbau <muhammad.ismail.azmi@mail.ugm.ac.id>
2024-10-30 11:01:29 +0700

    fix: rename

--- frontend/src/app/profile/page.js
+++ frontend/src/app/profile/page.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../../components/header';
 import ProfileSettings from './profilesettings';
 
-function WasteManagementDashboard() {
+function ProfilePage() {
     return (
         <div className="flex flex-col bg-white">
             <div className="flex overflow-hidden flex-col w-full bg-white min-h-[800px] max-md:max-w-full">
@@ -15,4 +15,4 @@ function WasteManagementDashboard() {
     );
 }
 
-export default WasteManagementDashboard;
+export default ProfilePage;
